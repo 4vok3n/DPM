66c9ac3138ee116662cf7d00db9a8f71f8da3336
4vok3n <wjf@wjfcloud.co.uk>
2025-07-17 11:13:07 +0100

    Rebuild

--- js/modules/config.js
+++
@@ -1,6 +0,0 @@
-// Configuration settings for the application
-export const config = {
-  storageKey: 'datacenterPowerData',
-  defaultReportTitle: 'Datacenter Power Report',
-  dateFormat: 'YYYY-MM-DD' // Format used for date display
-};
